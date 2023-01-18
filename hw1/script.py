38df5a222863f695895f3b9d6cd3ea459d11a5bc
Greg Tystahl <gttystah@tystahl.csc.ncsu.edu>
2023-01-18 15:40:47 -0500

    Finished the conversion and gets it right on set seed. Needs cleaning

--- hw1/script.py
+++ hw1/script.py
@@ -9,6 +9,7 @@
 #                                      \/_/          
 import re
 import sys
+import math
 
 the,Help = {},"""
 script.lua : an example script with help text and a test suite
@@ -24,14 +25,123 @@ OPTIONS:
 
 ACTIONS:
 """
-dict.__str__
+# print(the, Help)
 
-print(the, Help)
+class SYM():
+  def __init__(self):
+    self.n = 0
+    self.has = {}
+    self.most = 0
+    self.mode = None
 
-# TODO Line 24 with the env stuff
+  def add(self, x):
+    if x != "?":
+      self.n = self.n + 1
+      if x in self.has.keys():
+        self.has[x] += 1
+      else:
+        self.has[x] = 1
+      if self.has[x] > self.most:
+        self.most = self.has[x]
+        self.mode = x
+
+  def mid(self, x=0):
+    return self.mode
+
+  def div(self, x=0):
+    def fun(p):
+      return p * math.log(p, 2)
+
+    e = 0
+    for _,n in self.has.items():
+      e = e + fun(n / self.n)
+
+    return -e
+    
+class NUM():
+  def __init__(self):
+    self.n = 0
+    self.mu = 0
+    self.m2 = 0
+    self.lo = -math.inf
+    self.hi = math.inf
+
+  def add(self, n):
+    if n != "?":
+      self.n += 1
+      d = n - self.mu
+      self.mu = self.mu + d/self.n
+      self.m2 = self.m2 + d*(n - self.mu)
+      self.lo = min(n, self.lo)
+      self.hi = max(n, self.hi)
+
+  def mid(self, x=0):
+    return self.mu
+
+  def div(self, x=0):
+    # Ouf
+    return (self.m2 < 0 or self.n < 2) and 0 or (self.m2 / (self.n - 1)) ** 0.5
+
+Seed = 927162211
+def rint(lo, hi):
+  return math.floor(0.5 + rand(lo, hi))
+
+def rand(lo, hi):
+  global Seed
+
+  if not lo:
+    lo = 0
+  
+  if not hi:
+    hi = 1
+
+  Seed = (16807 * Seed) % 2147483647
+
+  return lo + (hi - lo) * Seed / 2147483647
+
+def rnd(n, nPlaces=0):
+  mult = 0
+  
+  if nPlaces:
+    mult = 10 ** nPlaces
+  else:
+    mult = 10 ** 3
+
+  return math.floor(n * mult + 0.5) / mult
+  
 
 def oo(t):
   print(t)
+
+def generator():
+  global Seed
+  num1 = NUM()
+  num2 = NUM()
+
+  Seed = the["seed"]
+  for i in range(1, 10 ** 3):
+    num1.add( rand(0, 1) )
+  Seed = the["seed"]
+  for i in range(1, 10 ** 3):
+    num2.add( rand(0, 1) )
+
+  m1 = rnd(num1.mid(), 10)
+  m2 = rnd(num2.mid(), 10)
+  return m1 == m2 and .5 == rnd(m1, 1)
+
+def checkSyms():
+  sym = SYM()
+  for x in ["a","a","a","a","b","b","c"]:
+    sym.add(x)
+  # print(sym.mid())
+  # print(rnd(sym.div()))
+  return "a" == sym.mid() and 1.379 == rnd(sym.div())
+
+def checkNums():
+  num = NUM()
+  for x in [1,1,1,1,2,2,3]:
+    num.add(x)
+  return 11/7 == num.mid() and 0.787 == rnd(num.div())
 
 def coerce(s):
   def fun(s1):
@@ -56,11 +166,9 @@ def coerce(s):
 
 def settings(s):
   t={}
-  # s:gsub("\n[%s]+[-][%S]+[%s]+[-][-]([%S]+)[^\n]+= ([%S]+)",function(k,v) t[k]=coerce(v) end)
   res = re.findall("\n[\s]+[-][\S]+[\s]+[-][-]([\S]+)[^\n]+= ([\S]+)", s)
   for k,v in res:
     t[k] = coerce(v)
-  # res = re.findall("\n[%s]", s)
   return t
 
 def cli(options):
@@ -85,7 +193,7 @@ def cli(options):
 def main(options: dict, HELP, funs):
     global Seed
     saved, fails = {}, 0
-    for k,v in cli(settings(HELP)).items(): # Dont think this is exactly right
+    for k,v in cli(settings(HELP)).items(): 
       options[k] = v
       saved[k] = v
     if options["help"]:
@@ -101,7 +209,6 @@ def main(options: dict, HELP, funs):
             print("❌ fail:",what)
           else:
             print("✅ pass:",what)
-    # Need the b4 stuff here too
     exit(fails)
 
 egs = {}
@@ -111,5 +218,11 @@ def eg(key,string,fun):
   Help = Help + ("  -g  %s\t%s\n" % (key,string))
 
 eg("the", "show settings", lambda: oo(the))
+
+eg("rand", "generate, reset, regenerate same", generator)
+
+eg("sym", "check syms", checkSyms)
+
+eg("num", "check nums", checkNums)
 
 main(the, Help, egs)
